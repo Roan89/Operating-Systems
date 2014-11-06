7a1066a54c787c2de6b93cac5acfa03fb06c961c
Rohit Kiran <rohitsree7@gmail.com>
2014-11-06 15:29:51 +0530

    Update readwrite.c

--- readwrite.c
+++ readwrite.c
@@ -1,38 +1,24 @@
-int NumberofReaderThread=atoi(argv[1]);
-int NumberofWriterThread=atoi(argv[2]);
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
 
- for(i=0;i<NumberofReaderThread;i++)
- {
-  pthread_create(&Readers_thr[i],NULL,Reader,(void *)i);
- }
- for(i=0;i<NumberofWriterThread;i++)
- { 
-  pthread_create(&Writer_thr[i],NULL,Writer,(void *)i);
- }
- for(i=0;i<NumberofWriterThread;i++)
- {
-  pthread_join(Writer_thr[i],NULL);
- }
+sem_t rcountacc;
+sem_t dbcountacc;
+int rcount=0;
 
- for(i=0;i<NumberofReaderThread;i++)
- {
-  pthread_join(Readers_thr[i],NULL); 
- }
- sem_destroy(&databaseAccess);
- sem_destroy(&readCountAccess); 
- return 0;
-}
+void *Reader(void *arg);
+void *Writer(void *arg);
 
-void * Writer(void *arg)
+void *Writer(void *arg)
 {
  
  sleep(1); 
  int temp=(int)arg;
  printf("\nWriter %d is trying to enter into database for modifying the data",temp);
- sem_wait(&databaseAccess);
+ sem_wait(&dbcountacc);
  printf("\nWriter %d is writting into the database",temp); 
  printf("\nWriter %d is leaving the database\n",temp); 
- sem_post(&databaseAccess);
+ sem_post(&dbcountacc);
 }
 
 void *Reader(void *arg)
@@ -40,21 +26,52 @@ void *Reader(void *arg)
  sleep(1); 
  int temp=(int)arg;
  printf("\nReader %d is trying to enter into the Database for reading the data",temp);
- sem_wait(&readCountAccess);
- readCount++;
- if(readCount==1)
+ sem_wait(&rcountacc);
+ rcount++;
+ if(rcount==1)
  {
-  sem_wait(&databaseAccess);
+  sem_wait(&dbcountacc);
   printf("\nReader %d is reading the database",temp); 
  }
- sem_post(&readCountAccess);
- sem_wait(&readCountAccess);
- readCount--;
- if(readCount==0)
+ sem_post(&rcountacc);
+ sem_wait(&rcountacc);
+ rcount--;
+ if(rcount==0)
  {
   printf("\nReader %d is leaving the database\n",temp);  
-  sem_post(&databaseAccess);
+  sem_post(&dbcountacc);
  }
- sem_post(&readCountAccess);
+ sem_post(&rcountacc);
 }
+int main(int argc, char *argv[])
+{
+ int i=0;
+ sem_init(&rcountacc,0,1);
+ sem_init(&dbcountacc,0,1);
+ 
+ pthread_t Readers_thr[100],Writer_thr[100];
 
+int NoRT=atoi(argv[1]);
+int NoWT=atoi(argv[2]);
+
+ for(i=0;i<NoRT;i++)
+ {
+  pthread_create(&Readers_thr[i],NULL,Reader,(void *)i);
+ }
+ for(i=0;i<NoWT;i++)
+ { 
+  pthread_create(&Writer_thr[i],NULL,Writer,(void *)i);
+ }
+ for(i=0;i<NoWT;i++)
+ {
+  pthread_join(Writer_thr[i],NULL);
+ }
+
+ for(i=0;i<NoRT;i++)
+ {
+  pthread_join(Readers_thr[i],NULL); 
+ }
+ sem_destroy(&dbcountacc);
+ sem_destroy(&rcountacc); 
+ return 0;
+}
